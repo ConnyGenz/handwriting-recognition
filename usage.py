a9185e77359087f4f3b02487c9a6560df5edc598
ConnyGenz <92215137+ConnyGenz@users.noreply.github.com>
2023-03-19 15:09:48 +0100

    Add files via upload

--- usage.py
+++ usage.py
@@ -34,10 +34,10 @@ args = parser.parse_args()
 
 #%% Variables
 
-train_size = 15000
+train_size = 10000
 valid_size = 3000
 test_size = 300
-num_epochs = 60
+num_epochs = 50
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
 # character to number
